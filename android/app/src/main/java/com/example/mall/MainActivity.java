073e4c2dada842426120b55db44be09fae5329f4
lish <850716183@qq.com>
2021-04-11 01:55:34 +0800

    第一次

--- android/app/src/main/java/com/example/mall/MainActivity.java
+++ android/app/src/main/java/com/example/mall/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.mall;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
