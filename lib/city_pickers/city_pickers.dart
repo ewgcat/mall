ee4fb2e9b1476f06a45a819fb3a6654d1239e496
lish <850716183@qq.com>
2021-04-11 01:31:10 +0800

    first

---
+++ lib/city_pickers/city_pickers.dart
@@ -0,0 +1,9 @@
+library city_pickers;
+
+export 'modal/point.dart';
+export 'modal/result.dart';
+export 'src/cities_selector/cities_style.dart';
+export 'src/cities_selector/utils.dart';
+export 'src/city_picker.dart';
+export 'src/show_types.dart';
+export 'src/utils/index.dart';
---
+++ lib/city_pickers/src/utils/index.dart
@@ -0,0 +1,18 @@
+import 'package:mall/city_pickers/modal/result.dart';
+
+import 'location.dart';
+
+class CityPickerUtil {
+  Map<String, dynamic> citiesData;
+  Map<String, dynamic> provincesData;
+
+  CityPickerUtil({this.citiesData, this.provincesData})
+      : assert(citiesData != null),
+        assert(provincesData != null);
+
+  Result getAreaResultByCode(String code) {
+    Location location =
+        new Location(citiesData: citiesData, provincesData: provincesData);
+    return location.initLocation(code);
+  }
+}
